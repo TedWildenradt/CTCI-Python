1cdb3e6ff49c63bd5ccef7f37a73ea6dd8c5e20f
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-13 21:18:57 -0800

    Remove duplicates in linkedlist

--- Linked_Lists/remove_dups.py
+++ Linked_Lists/remove_dups.py
@@ -2,13 +2,28 @@ class LinkNode:
   def __init__(self,val):
     self.val = val
     self.next = None 
+arr = [2,2,4,4,5,6]
 headNode = LinkNode(1)
 curr = headNode 
-i = 2
-for i in range(2,7):
+for i in arr:
   node = LinkNode(i)
   curr.next = node 
   curr = curr.next 
-  node = LinkNode(i*2)
-  curr.next = node
-  curr = curr.next
+
+
+def remove_dups(head):
+  visited = set()
+  p1 = head 
+  prev = None
+  while p1:
+    if p1.val in visited:
+      prev.next = p1.next
+      p1 = p1.next
+    else:
+      visited.add(p1.val) 
+      prev = p1 
+      p1 = p1.next
+  return head 
+
+remove_dups(headNode)
+
