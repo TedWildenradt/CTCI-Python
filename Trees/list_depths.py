1e0a9e940ae469cdb5bb21257ae0b83e8f46ce58
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-17 13:13:18 -0800

    list of depths function

--- Trees/list_depths.py
+++ Trees/list_depths.py
@@ -15,3 +15,26 @@ root.right = TreeNode(2)
 root.left.left = TreeNode(3)
 root.left.right = TreeNode(4)
 
+def list_of_depths(root):
+  if not root:
+    return None
+  output = [LinkNode(root.val)]
+  level = [root]
+  while level:
+    new_level = []
+    for node in level:
+      if node.left:
+        new_level.append(node.left)
+      if node.right:
+        new_level.append(node.right)
+    head = LinkNode(0)
+    curr = head 
+    for node in new_level:
+      link = LinkNode(node.val)
+      curr.next = link 
+      curr = curr.next 
+    output.append(head.next)
+    level = new_level
+  return output[:-1] 
+
+list_of_depths(root)
