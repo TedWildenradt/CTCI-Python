917f4aaa2040e971bc2247cf6512d0e2ca37db18
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-17 12:26:44 -0800

    Create tree for testing

--- Trees/list_depths.py
+++ Trees/list_depths.py
@@ -8,3 +8,10 @@ class LinkNode:
   def __init__(self,val):
     self.val = val
     self.next = None
+
+root = TreeNode(0)
+root.left = TreeNode(1)
+root.right = TreeNode(2)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(4)
+
