81b53179f44d8b4dd85d5d90fa70ad3d2272b3ca
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-17 12:23:34 -0800

    Setup TreeNode and LinkNode classes

---
+++ Trees/list_depths.py
@@ -0,0 +1,10 @@
+class TreeNode:
+  def __init__(self,val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+class LinkNode:
+  def __init__(self,val):
+    self.val = val
+    self.next = None
