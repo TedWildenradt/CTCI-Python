9e0fd106fb1d563f8f294f93b3fb51a4864ce535
TedWildenradt <tedkwildenradt@gmail.com>
2019-02-18 15:19:54 -0800

    New move positions and trace back path functions

--- Knight.rb
+++ Knight.rb
@@ -13,7 +13,7 @@ class Knight
     [ 2,  1]
   ]
 
-  def valid_moves(pos)
+  def self.valid_moves(pos)
     moves = []
     cur_x,cur_y = pos 
     MOVES.each do |dx,dy|
@@ -25,4 +25,46 @@ class Knight
     moves 
   end 
 
-  
+  def initialize(start_pos)
+    @start_pos = start_pos
+    @considered = [start_pos]
+    build_move_tree
+  end 
+
+  def find_path(end_pos)
+    end_node = root_node.dfs(end_pos)
+    trace_path_back(end_node).reverse.map(&:value)
+  end
+
+  private
+  attr_accessor :root_node, :considered
+
+  def build_move_tree 
+    self.root_node = PolyTreeNode.new(start_pos)
+    nodes = [root_node]
+    until nodes.empty?
+      current_node = nodes.shift
+      current_pos = current_node.val 
+      new_move_positions(current_pos).each do |next_pos|
+        next_node = PolyTreeNode.new(next_pos)
+        current_node.add_child(next_node)
+        nodes << next_node
+      end 
+    end  
+  end
+
+  def new_move_positions(pos)
+    Knight.valid_moves(pos)
+    .reject {|new_pos| considered.include?(new_pos)}
+    .each { |new_pos| considered << new_pos}
+  end 
+
+  def trace_path_back(end_node)
+    nodes = []
+    current_node = end_node
+    until current_node.nil?
+      nodes << current_node
+      current_node = current_node.parent
+    end 
+    return nodes
+  end 
