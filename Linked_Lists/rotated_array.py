defef893b9abca5c2cf4dad932cf9fca8037710e
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-03 12:17:42 -0800

    Binary search rotated array

---
+++ Linked_Lists/rotated_array.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        start = 0
+        end = len(nums) - 1
+        
+        while start <= end:
+            mid = (start + end) // 2
+            midValue = nums[mid]
+            if midValue == target:
+                return mid
+            if nums[start] <= midValue:
+                if nums[start] <= target < midValue:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                if midValue < target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1
+
+obj = Solution()
+obj.search([4,5,6,7,0,1,2], 0)
