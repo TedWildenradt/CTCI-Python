14ec330f7efc3d0759bf749c4aa2e4255eb6c6e5
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-03 17:57:22 -0800

    Lowest positive integer in array function

---
+++ lowest_positive.py
@@ -0,0 +1,17 @@
+def lowestPositive(array):
+  j = 0
+  for i in range(len(array)):
+    if array[i] <= 0:
+      array[j], array[i] = array[i], array[j]
+      j += 1
+      
+  for i in range(len(array)):
+    if abs(array[i]) - 1 < len(array) and array[abs(array[i]) - 1 > 0]:
+      array[abs(array[i]) - 1] *= -1
+
+  for i in range(len(array)):
+    if array[i] > 0:
+      return i + 1
+
+
+lowestPositive([3, 4, -1, 1])
