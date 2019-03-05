e77900a7e900a9273e30a0f6307ec1dcb8368a15
TedWildenradt <tedkwildenradt@gmail.com>
2019-03-05 14:16:30 -0800

    longest consecutive sequence

---
+++ Recursion-Dynamic-Programming/longest_consecutive_sequence.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def longestConsecutive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        numSet = set(nums)
+        
+        output = 0
+        
+        for num in nums:
+            count = 1
+            if (num - 1) not in numSet:
+                while num + 1 in numSet:
+                    count += 1
+                    num += 1
+            output = max(output, count)
+
+obj = Solution()
+obj.longestConsecutive([100, 4, 200, 1, 3, 2])
